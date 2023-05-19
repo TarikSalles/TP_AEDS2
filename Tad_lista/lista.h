c03eea911de2782cdcece21fe8737d812a933e5b
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2023-05-19 10:25:42 -0300

    Adicionado tad lista nao encadeado

---
+++ Tad_lista/lista.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define Max_tam 10
+
+typedef struct celula{
+    char index_arquivo;
+    int num_ocorrencias;
+}celula;
+
+typedef struct Tlista * Aponta_lista;
+
+typedef struct Tlista{
+    celula lista[Max_tam];
+    int inicio, fim;
+}Tlista;
+
+
+int Inicializa(Tlista * lista);
+int Insere(Tlista * lista, char index);
+int Lista_cheia(Tlista * lista);
+int Imprime_lista(Tlista * lista);
