afe860982b536176659a01a32216211d9a347c92
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2023-05-27 14:43:13 -0300

    Feito encapsulamento da funcao de leitura e respectivos comentarios

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 
 int Inicializa_geral(Arvore * raiz, Tdocumento * documento, TBusca * busca);
+int Leitura(FILE * arq, Arvore * raiz, Tdocumento * documento);
 
 int main(){
     //Cabecalho dos tads
@@ -12,39 +13,67 @@ int main(){
     TBusca busca;
     FILE * arq;
 
-    //cabecalhos da main
-    char path_arquivo[300], string[50] , string_aux[50], string_nome_arquivo[50];
-    const char string_fixa_nome_arquivo[50] = "arquivo";
-    int index_do_arquivo = 0;
-    int contador = -1, tam, c; //auxilia na verificação de qual arquivo se está lendo
-    int index_convertido;
-
-
     // ? Funcao de inicializaçao
     Inicializa_geral(&raiz, &documento, &busca);
 
+    // ? Funcao de leitura
+    Leitura(arq, &raiz, &documento);
+
+
+    Ordem(raiz);
+
+    return 0;
+}
+
+int Inicializa_geral(Arvore * raiz, Tdocumento * documento, TBusca * busca){
+    Inicializar_Arvore(raiz);
+    inicializaDoc(documento);
+    InicializaBusca(busca);
+    return 0;
+}
+
+int Leitura(FILE * arq, Arvore * raiz, Tdocumento * documento){
+    //cabecalhos de constantes na main
+    char path_arquivo[300], string[50] , string_tratada[50], string_nome_arquivo[50];
+    int index_do_arquivo = 0, contador = -1, tamanho_string, c; //auxilia na verificação de qual arquivo se está lendo
+    /** Explicações:
+     * string = guarda as strings provenientes da leitura do arquivo
+     * string_tratada = string sem ';'
+     * contador = conta o "estagio" da leitura do arquivo(necessario para saber quando trata-se de uma string ou de um index de arquivo)
+     * index_do_arquivo = autoexplicativo
+     * tamanho_string = auto_explicativo
+     */
     printf("Digite o path do arquivo:\n");
+
     // ! scanf("%s",&path_arquivo);
     strcpy(path_arquivo,"Data/sem_pontuacao_reduzido.txt");
 
+    //Abertura e verificacao do estado do arquivo
+    arq = fopen(path_arquivo, "r");
     if (arq == NULL){
         printf("Nao foi possivel abrir o arquivo!\n");
         return 1;
     }
 
-    arq = fopen(path_arquivo, "r");
-
     while(fscanf(arq,"%s",string) != EOF){
-        tam = strlen(string);
-        //printf("%d %c", tam, string[tam-1]);
-        if (string[tam-1] == ';'){
-            contador = (contador + 1)%3;
-            //printf("Contador: %d\n", contador);
-            for (c = 0; c < (tam - 1); c++){
-                string_aux[c] = string[c];
+
+        tamanho_string = strlen(string);
+
+        if (string[tamanho_string-1] == ';'){
+            //Cada arquivo tem 3 partes: Index, titulo e abstract. Isso explica o porque do mod 3
+            contador = (contador + 1)%3; 
+
+            //remove o ';' da string:
+            for (c = 0; c < (tamanho_string - 1); c++){
+                string_tratada[c] = string[c];
             }
-            string_aux[tam-1] = '\0';
-            strcpy(string, string_aux);
+            //Substitui o ';' pelo '\0'
+            string_tratada[tamanho_string-1] = '\0';
+
+            //Atualiza string para ser a string tratada
+            strcpy(string, string_tratada);
+
+            //caso esteja no estado de leitura do index:
             if (contador == 0){
                 strcpy(string_nome_arquivo, "arquivo");
                 strcat(string_nome_arquivo, string);
@@ -54,18 +83,8 @@ int main(){
             }
 
         }
-        //printf("string: %s \nindex: %d\n\n", string, index_do_arquivo);
-        Insere_Palavra_Arvore(&raiz, string , index_do_arquivo, string_nome_arquivo, &documento);
+        Insere_Palavra_Arvore(raiz, string , index_do_arquivo, string_nome_arquivo, documento);
     }
-    Ordem(raiz);
-
     fclose(arq);
-    return 0;
-}
-
-int Inicializa_geral(Arvore * raiz, Tdocumento * documento, TBusca * busca){
-    Inicializar_Arvore(raiz);
-    inicializaDoc(documento);
-    InicializaBusca(busca);
     return 0;
 }
