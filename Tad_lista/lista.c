65d64eed756a9b970c7d25c264356e116e2b0e7a
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2023-05-27 11:34:57 -0300

    Implementado lista inserindo ordenado

--- Tad_lista/lista.c
+++ Tad_lista/lista.c
@@ -3,11 +3,14 @@
 #include "lista.h"
 
 int Inicializa(Tlista * lista){
-    lista->primeiro = (Aponta_lista)malloc(sizeof(TCelula));
-    if (lista->primeiro){
-        lista->ultimo = lista->primeiro;
-        lista->ultimo->prox = NULL;
+    Aponta_lista celula_aux = (Aponta_lista)malloc(sizeof(TCelula));
+    if(celula_aux == NULL){
+        printf("Problemas de alocacao de memoria\n");
+        return 1;
     }
+    lista->primeiro = celula_aux;
+    lista->ultimo = celula_aux;
+    lista->ultimo->prox = NULL;
 }
 
 int Lista_vazia(Tlista * lista){
@@ -15,6 +18,75 @@ int Lista_vazia(Tlista * lista){
 }
 
 int Insere(Tlista * lista, int index){
+    
+    Aponta_lista aux, celula_aux;
+    
+    aux = lista->primeiro;
+    if (Lista_vazia(lista)){
+        lista->ultimo->prox = (Aponta_lista)malloc(sizeof(TCelula));
+        lista->ultimo = lista->ultimo->prox;
+        lista->ultimo->index_arquivo = index;
+        lista->ultimo->num_ocorrencias = 1;
+        lista->ultimo->prox = NULL;
+        return 0;
+    }
+    while(1){
+        
+        if (aux->prox == NULL){
+            aux->prox = (Aponta_lista)malloc(sizeof(TCelula));
+            aux->prox->index_arquivo = index;
+            aux->prox->num_ocorrencias = 1;
+            aux->prox->prox = NULL;
+            lista->ultimo = aux->prox;
+            return 0;
+        }
+        if (aux->prox->index_arquivo == index){
+            aux->prox->num_ocorrencias += 1;
+            //printf("index == aux\n");
+            //printf("< %d , %d >\n", lista->ultimo->num_ocorrencias, lista->ultimo->index_arquivo);
+            return 0;
+        }
+        if (aux->prox->index_arquivo < index){
+            aux = aux->prox;
+            continue;
+        }
+        if (aux->prox->index_arquivo > index){
+            celula_aux = (Aponta_lista)malloc(sizeof(TCelula));
+            if(celula_aux == NULL){
+                printf("Problemas de alocacao de memoria\n");
+                return 1;
+            }
+            celula_aux->index_arquivo = index;
+            celula_aux->num_ocorrencias = 1;
+            celula_aux->prox = aux->prox;
+            aux->prox = celula_aux;
+            return 0;
+        }
+    }
+    return 0;
+}
+
+
+int Insere_novo(Tlista * lista, int index){
+    Aponta_lista aux;
+    
+    aux = lista->primeiro->prox;
+    while(aux != NULL){
+        if (aux->index_arquivo == index){
+            aux->num_ocorrencias += 1;
+            return 0;
+        }
+        aux = aux->prox;
+    }
+    lista->ultimo->prox = (Aponta_lista)malloc(sizeof(TCelula));
+    lista->ultimo = lista->ultimo->prox;
+    lista->ultimo->index_arquivo = index;
+    lista->ultimo->num_ocorrencias = 1;
+    lista->ultimo->prox = NULL;
+    return 1;
+}
+
+int Insere_original(Tlista * lista, int index){
     Aponta_lista aux;
     
     aux = lista->primeiro->prox;
@@ -35,6 +107,10 @@ int Insere(Tlista * lista, int index){
 
 int Imprime_lista(Tlista * lista){
     Aponta_lista aux;
+    if (Lista_vazia(lista)){
+        printf("Essa lista e vazia\n");
+        return 1;
+    }
     aux = lista->primeiro->prox;
     printf("Tuplas < qtde, idDoc >:\n");
     while(aux != NULL){
